1c1575f26c550a76b3505d219f4bdfeb1fd1121e
Roelie <roeland.bosch.1998@gmail.com>
2022-07-07 14:11:01 +0200

    Add dist

---
+++ dist/bin/utils.d.ts
@@ -0,0 +1,35 @@
+export function setPersistence(persistence_: {
+    bindState: (arg0: string, arg1: WSSharedDoc) => void;
+    writeState: (arg0: string, arg1: WSSharedDoc) => Promise<any>;
+    provider: any;
+} | null): void;
+export function getPersistence(): null | {
+    bindState: (arg0: string, arg1: WSSharedDoc) => void;
+    writeState: (arg0: string, arg1: WSSharedDoc) => Promise<any>;
+} | null;
+/**
+ * @type {Map<string,WSSharedDoc>}
+ */
+export const docs: Map<string, WSSharedDoc>;
+export class WSSharedDoc extends Y.Doc {
+    /**
+     * @param {string} name
+     */
+    constructor(name: string);
+    name: string;
+    mux: mutex.mutex;
+    /**
+     * Maps from conn to set of controlled user ids. Delete all user ids from awareness when this conn is closed
+     * @type {Map<Object, Set<number>>}
+     */
+    conns: Map<any, Set<number>>;
+    /**
+     * @type {awarenessProtocol.Awareness}
+     */
+    awareness: awarenessProtocol.Awareness;
+}
+export function getYDoc(docname: string, gc?: boolean): WSSharedDoc;
+export function setupWSConnection(conn: any, req: any, { docName, gc }?: any): void;
+import * as Y from "yjs";
+import * as mutex from "lib0/mutex";
+import * as awarenessProtocol from "y-protocols/awareness";
